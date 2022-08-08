acb634236467ffd5e3a68f818b849ba7f06d682a
payal9876 <payalyadav9456444@gmail.com>
2022-08-08 11:47:14 +0530

    second commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app=Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 
 def starting_model():
-    return render_template("index1.html")
+    return render_template("index.html")
 
 def prediction():
 
